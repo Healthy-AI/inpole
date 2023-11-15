a2a3582cd748a665a4e5924324824aff66581e69
antmats <anton.mattsson94@gmail.com>
2023-11-15 21:40:36 +0100

    add further comments

--- env_setup.sh
+++ env_setup.sh
@@ -3,20 +3,23 @@
 # Activate the base environment.
 conda activate
 
-# Remove the environment if it already exists.
+# Remove the inpole environment if it already exists.
 env_name=$(head -1 environment.yml | cut -d' ' -f2)
 if conda info --envs | grep -q $env_name; then conda remove -y --name $env_name --all; fi
 
-# Create the environment.
+# Create and activate the inpole environment.
 conda env create -f environment.yml
 conda activate $env_name
 
 # Install risk-slim and AIX360 in editable mode.
 #
-# Note: It is not possible to install these packages from the environment.yml
-# file while controlling the installation path.
+# These packages cannot be installed using Poetry. We could install them from
+# the environment.yml file, but then we would first need to clone the GitHub
+# repositories to a local directory. It is not possible to install these 
+# packages from the environment.yml file while controlling the installation 
+# path using the --src option.
 pip install -e git+https://github.com/antmats/risk-slim.git#egg=riskslim
 pip install -e git+https://github.com/Trusted-AI/AIX360.git#egg=aix360[rbm]
 
-# Install other packages using Poetry.
+# Install the remaining packages using Poetry.
 poetry install
