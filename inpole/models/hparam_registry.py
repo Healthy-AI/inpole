c8c928628dd1a2efe39ad0caa09fce2c35598264
antmats <anton.mattsson94@gmail.com>
2024-02-26 19:53:34 +0100

    update hparam registry

--- inpole/models/hparam_registry.py
+++ inpole/models/hparam_registry.py
@@ -5,7 +5,6 @@ from .. import OTHER_ESTIMATORS
 
 
 # @TODO: Add hyperparameters for the following estimators and datasets:
-# - rulefit
 # - fasterrisk
 
 
@@ -23,7 +22,7 @@ def _hparams(estimator_name, experiment, seed):
     # =========================================================================
     # Experiment-INDEPENDENT parameters.
     # =========================================================================
-     
+    
     if estimator_name in ['sdt', 'rdt', 'truncated_rdt']:
         _hparam('initial_depth', 2, lambda r: r.choice([1, 2]))
         _hparam('lambda_', 1.0e-3, lambda r: 10. ** r.choice([-3, -2, -1]))
@@ -37,7 +36,7 @@ def _hparams(estimator_name, experiment, seed):
     
     if estimator_name in ['pronet', 'prosenet', 'truncated_prosenet']:
         _hparam('d_min', 1, lambda r: r.choice([1, 2, 3, 4, 5]))
-        _hparam('lambda_div', 1.0e-3, lambda r: 10. ** r.choice([-5, -4, -3, -2, -1]))
+        _hparam('lambda_div', 1.0e-3, lambda r: 10. ** r.choice([-5, -4, -3, -2, -1, 0]))
         
     if estimator_name in ['mlp', 'pronet']:
         hidden_dims = np.array([(16,), (32,), (64,), (16, 16), (32, 32), (64, 64)], dtype=object)
@@ -55,6 +54,8 @@ def _hparams(estimator_name, experiment, seed):
     
     if estimator_name == 'dt':
         _hparam('max_depth', None, lambda r: r.choice([3, 5, 7, 9, 11, 13, 15]))
+        _hparam('criterion', 'gini', lambda r: r.choice(['gini', 'entropy']))
+        _hparam('min_samples_split', 2, lambda r: r.choice([2, 4, 8, 16, 32, 64, 128]))
     
     if estimator_name == 'frl':
         _hparam('minsupport', 10, lambda r: r.choice([8, 10, 12, 16, 18, 20]))
@@ -65,10 +66,13 @@ def _hparams(estimator_name, experiment, seed):
         _hparam('max_coefficient', 5, lambda r: r.choice([3, 4, 5, 6, 7, 8]))
         _hparam('max_L0_value', 5, lambda r: r.choice([3, 4, 5, 6, 7]))
         _hparam('w_pos', 1, lambda r: r.choice([3, 4, 5]))
-    
+
     if estimator_name == 'rulefit':
         _hparam('tree_size', 4, lambda r: r.choice([1, 2, 3, 4, 5]))
-        _hparam('max_rules', 50, lambda r: r.choice([10, 30, 50, 70, 90, 100]))
+        _hparam('max_rules', 50, lambda r: r.choice([10, 20, 30, 40, 50, 60, 70, 80, 90, 100]))
+        _hparam('model_type', 'rl', lambda r: r.choice(['r', 'l', 'rl']))
+        _hparam('lin_standardise', True, lambda r: r.choice([True, False]))
+        _hparam('memory_par', 0.1, lambda r: r.choice([0.001, 0.01, 0.1]))
         
     # =========================================================================
     # Experiment-DEPENDENT parameters (RA/Switch).
@@ -108,7 +112,7 @@ def _hparams(estimator_name, experiment, seed):
     # =========================================================================
     
     if experiment == 'sepsis' and estimator_name not in OTHER_ESTIMATORS:
-        _hparam('lr', 1.0e-3, lambda r: 10. ** r.choice([-3, -2]))
+        _hparam('lr', 1.0e-3, lambda r: 10. ** r.choice([-3.5, -3, -2]))
         _hparam('max_epochs', 500, lambda r: 500)
 
     if experiment == 'sepsis' and estimator_name in ['sdt', 'mlp', 'pronet']:
@@ -121,11 +125,14 @@ def _hparams(estimator_name, experiment, seed):
         'truncated_rdt', 'truncated_rnn', 'truncated_prosenet'
     ]:
         _hparam('batch_size', 128, lambda r: r.choice([64, 128, 256]))
+    
+    if experiment == 'sepsis' and estimator_name in ['prosenet', 'truncated_prosenet']:
+        _hparam('module__encoder__num_layers', 1, lambda r: r.choice([1, 2, 3]))
 
     if experiment == 'sepsis' and estimator_name in [
         'pronet', 'prosenet', 'truncated_prosenet'
     ]:
-        _hparam('module__num_prototypes', 10, lambda r: r.choice([5, 10, 15, 20, 25]))
+        _hparam('module__num_prototypes', 10, lambda r: r.choice([5, 10, 15, 20, 25, 30]))
 
     return hparams
 
