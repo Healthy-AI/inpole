4233f07e35518e747c8bb13ad6103342f4c44000
antmats <anton.mattsson94@gmail.com>
2024-10-14 22:20:45 +0200

    add ra preprocessing

--- inpole/data/preprocessing.py
+++ inpole/data/preprocessing.py
@@ -1,8 +1,26 @@
 import os
+import warnings
+import itertools
 
 import numpy as np
 import pandas as pd
 
+try:
+    from .corevitas import COREVITAS_DATA
+except ImportError:
+    import os
+    import pickle
+    if not os.environ.get("COREVITAS_DATA_PATH"):
+        raise ValueError(
+            "The environment variable 'COREVITAS_DATA_PATH' is not set."
+        )
+    with open(os.environ("COREVITAS_DATA_PATH"), "rb") as f:
+        COREVITAS_DATA = pickle.load(f)
+
+
+# =============================================================================
+# == ADNI =====================================================================
+# =============================================================================
 
 def make_adni(adni_path, out_path):
     """Preprocess the ADNI dataset as described in Pace et al. (2022)."""
@@ -70,6 +88,247 @@ def make_adni(adni_path, out_path):
     file_name = os.path.join(out_path, 'adni.pkl')
     adni.to_pickle(file_name)
 
+
+# =============================================================================
+# == RA =======================================================================
+# =============================================================================
+
+def get_all_variables():
+    all_variables = {}
+    drugs = itertools.chain.from_iterable(COREVITAS_DATA.drug_classes.values())
+    for v, i in COREVITAS_DATA.variables.items():
+        if "%s" in v:
+            for d in drugs:
+                all_variables[v % d] = i
+        else:
+            all_variables[v] = i
+    return all_variables
+
+
+def categorize_bp(row):
+    # https://www.heart.org/en/health-topics/high-blood-pressure
+    if row.seatedbp1 > 180 or row.seatedbp2 > 120:
+        return 'htn stage 3'
+    elif row.seatedbp1 >= 140 or row.seatedbp2 >= 90:
+        return 'htn stage 2'
+    elif row.seatedbp1 >= 130 or row.seatedbp2 >= 80:
+        return 'htn stage 1'
+    elif 120 <= row.seatedbp1 < 130 and row.seatedbp2 < 80:
+        return 'elevated'
+    elif row.seatedbp1 < 120 and row.seatedbp2 < 80:
+        return 'normal'
+    else:
+        return np.nan
+
+
+def summarize(X, name, impute=False):
+    if impute:
+        return pd.Series(X.sum(axis=1) > 0, name=name, dtype='boolean')
+    else:
+        X = X.sum(axis=1)
+        X = X.where(X.isna(), X > 0)
+        X.name = name
+        return X.astype('boolean')
+
+
+def assign_stage(X):
+    """Assign the stage of treatment."""
+
+    def update_stage(x):
+        try:
+            i = x[x == 1].index[0]
+        except IndexError:
+            return x
+        j = x.index[-1]
+        x.loc[i:j] = range(1, 2 + j-i)
+        return x
+
+    if 'stage' in X.columns:
+        X = X.drop(columns='stage')
+
+    X = X.assign(stage=-1)
+
+    is_baseline = (X.hxbiojak == 0) & (X.initbiojak == 1)
+    X.loc[is_baseline, 'stage'] = 1
+
+    X['stage'] = X.groupby('id', group_keys=False).stage.apply(update_stage)
+
+    return X.astype({'stage': 'float64'})
+
+
+def require_valid_therapies(x):
+    if not x.stage.eq(1).any():
+        return x
+    idx_first_invalid = x[
+        x.stage.ge(1) & x.therapy.eq("Other therapy")
+    ].first_valid_index()
+    return x if idx_first_invalid is None else x.loc[:idx_first_invalid-1]
+
+
+def make_ra(ra_path, out_path, impute=True):
+    # Load the preprocessed RA dataset.
+    ra = pd.read_pickle(ra_path)
+
+    # Assign the stage of treatment.
+    ra = pd.concat(
+        [
+            assign_stage(ra[ra.visitdate.notna()]),
+            ra[ra.visitdate.isna()]
+        ]
+    ).sort_values(by=['index', 'date'])
+
+    # Filter out patients without any index visit.
+    eligible_ids = ra[ra.stage.eq(1)].id.unique()
+    ra = ra[ra.id.isin(eligible_ids)]
+
+    # Filter out patients with invalid therapies.
+    ra = ra.groupby('id', group_keys=False).apply(require_valid_therapies)
+
+    # Filter out patients with no follow-up visits.
+    has_followup = ra[ra.stage.ge(1)].groupby('id').size() > 1
+    eligible_ids = has_followup[has_followup].index
+    ra = ra[ra.id.isin(eligible_ids)]
+
+    # Decode categorical variables.
+    for v, i in get_all_variables().items():
+        if v in ra.columns and 'categories' in i:
+            with warnings.catch_warnings():
+                warnings.simplefilter('ignore', FutureWarning)
+                ra.loc[:, v] = ra[v].map(i['categories'])
+
+    # Categorize BMI.
+    bins = [0, 18.4, 24.9, 29.9, np.inf]
+    ra['bmi_cat'] = pd.cut(
+        ra.bmi, bins=bins,
+        labels=['underweight', 'healthy', 'overweight', 'obesity'],
+        include_lowest=True
+    )
+
+    # Categorize CDAI.
+    bins = [0, 2.8, 10, 22, np.inf]
+    ra['cdai_cat'] = pd.cut(
+        ra.cdai, bins=bins, labels=['remission', 'low', 'moderate', 'high'],
+        include_lowest=True
+    )
+
+    # Categorize blood pressure.
+    with warnings.catch_warnings():
+        warnings.simplefilter('ignore', pd.errors.PerformanceWarning)
+        ra['bp_cat'] = ra.apply(categorize_bp, axis=1).astype('category')
+
+    # Include variables that should be imputed using forward filling (if `impute=True`).
+    c_ffill = [
+        'age', 'gender', 'college_completed', 'work_status', 'bmi_cat',
+        'bp_cat', 'smoker', 'drinker', 'insurance_private', 'insurance_medicare',
+        'insurance_medicaid', 'insurance_none', 'cdai_cat', 'duration_ra',
+    ]
+    X_ffill = ra[c_ffill]
+    if impute:
+        X_ffill = X_ffill.groupby(by=ra.id).fillna(method='ffill')
+
+    # Include indicators of pregnancy.
+    X_pregnancy = ra[['pregnant_since', 'pregnant_current']]
+
+    # Include indicators of RF and CCP positivity.
+    ccpos = ra.ccppos.astype('string')
+    rfpos = ra.rfpos.astype('string')
+    if impute:
+        ccpos = ccpos.fillna('Not tested')
+        rfpos = rfpos.fillna('Not tested')
+    to_replace = {'False': 'Negative', 'True': 'Positive'}
+    ccpos = ccpos.replace(to_replace).astype('category')
+    rfpos = rfpos.replace(to_replace).astype('category')
+    X_ra_biomarkers = pd.concat([ccpos, rfpos], axis=1)
+    X_ra_biomarkers = X_ra_biomarkers.rename(columns={'ccppos': 'ccp', 'rfpos': 'rf'})
+
+    # Include TB test results.
+    tb = ra.ppd.astype('string')
+    if impute:
+        tb = tb.fillna('Not tested')
+    to_replace = {'False': 'Negative', 'True': 'Positive'}
+    X_tb = tb.replace(to_replace).astype('category')
+
+    # Include variables indicating joint damage.
+    X_joints = ra[['erosdis', 'jtspnarrow', 'jtdeform']]
+
+    # Include variables indicating severe infections.
+    infections = ['hospinf', 'ivinf']
+    X_infections = summarize(ra[infections], name='infections', impute=impute)
+
+    # Include variables indicating comorbidities.
+
+    comor_metabolic = ['comor_hld', 'comor_diabetes']
+    X_comor_metabolic = summarize(ra[comor_metabolic], 'comor_metabolic', impute)
+
+    comor_cvd = [
+        'comor_htn_hosp', 'comor_htn', 'comor_revasc', 'comor_ven_arrhythm',
+        'comor_mi', 'comor_acs', 'comor_unstab_ang', 'comor_cor_art_dis',
+        'comor_chf_hosp', 'comor_chf_nohosp', 'comor_stroke', 'comor_tia',
+        'comor_card_arrest', 'comor_oth_clot', 'comor_pulm_emb',
+        'comor_pef_art_dis', 'comor_pat_event', 'comor_urg_par', 'comor_pi',
+        'comor_carotid', 'comor_other_cv',
+    ]
+    X_comor_cvd = summarize(ra[comor_cvd], 'comor_cvd', impute)
+
+    comor_respiratory = ['comor_copd', 'comor_asthma', 'comor_fib']
+    X_comor_respiratory = summarize(ra[comor_respiratory], 'comor_respiratory', impute)
+
+    comor_dil = ['comor_drug_ind_sle']
+    X_comor_dil = summarize(ra[comor_dil], 'comor_dil', impute)
+
+    comor_cancer = [
+        'comor_bc', 'comor_lc', 'comor_lymphoma', 'comor_skin_cancer_squa',
+        'comor_skin_cancer_mel', 'comor_oth_cancer',
+    ]
+    X_comor_cancer = summarize(ra[comor_cancer], 'comor_cancer', impute)
+
+    comor_gi_liver = [
+        'comor_ulcer', 'comor_bowel_perf', 'comor_hepatic_wbiop',
+        'comor_hepatic_nobiop',
+    ]
+    X_comor_gi_liver = summarize(ra[comor_gi_liver], 'comor_gi_liver', impute)
+
+    comor_musculoskeletal = ['sec_sjog', 'jt_deform']
+    to_replace = {'no': False, 'yes': True, 'new': True}
+    X_comor_musculoskeletal = summarize(
+        ra[comor_musculoskeletal].replace(to_replace).astype('boolean'),
+        'comor_musculoskeletal',
+        impute,
+    )
+
+    comor_other = [
+        'comor_psoriasis', 'comor_depression', 'comor_fm', 'comor_oth_neuro',
+        'comor_hemorg_hosp', 'comor_hemorg_nohosp', 'comor_oth_cond',
+    ]
+    X_comor_other = summarize(ra[comor_other], 'comor_other', impute)
+
+    X_comor = [
+        X_comor_metabolic, X_comor_cvd, X_comor_respiratory, X_comor_dil,
+        X_comor_cancer, X_comor_gi_liver, X_comor_musculoskeletal, X_comor_other
+    ]
+    X_comor = pd.concat(X_comor, axis=1)
+
+    # Combine all variables.
+    X = [
+        ra.date.dt.year.rename('year').astype('float64'), X_ffill, X_pregnancy,
+        X_ra_biomarkers, X_tb, X_joints, X_infections, X_comor, ra.stage,
+    ]
+    X = pd.concat(X, axis=1)
+
+    # Replace prebaseline therapies classified as "Other therapy" with
+    # "csDMARD therapy" to avoid introducing an unused category in the data
+    # pipeline.
+    assert not 'Other therapy' in ra.loc[ra.stage.ge(1), 'therapy']
+    ra.loc[ra.therapy.eq('Other therapy'), 'therapy'] = 'csDMARD therapy'
+
+    # Save the data.
+    Xgy = pd.concat([X, ra.therapy.astype('category'), ra.id], axis=1)
+    Xgy.to_pickle(os.path.join(out_path, 'ra.pkl'))
+
+
+# =============================================================================
+# == Sepsis ===================================================================
+# =============================================================================
 
 def get_NEWS2_score(respiratory_rate, SpO2, on_vent, blood_pressure, heart_rate, is_CVPU, temperature):
     """Calculate the National Early Warning 2 (NEWS2) score.
@@ -189,6 +448,10 @@ def make_sepsis(sepsis_path, out_path, exclude_patients_with_missing_data=False)
     file_name = os.path.join(out_path, 'sepsis.pkl')
     sepsis.to_pickle(file_name)
 
+
+# =============================================================================
+# == COPD =====================================================================
+# =============================================================================
 
 class COPDPreprocessing:
     def __init__(self, filename):
--- scripts/make_datasets.py
+++ scripts/make_datasets.py
@@ -1,18 +1,28 @@
 import argparse
 
-from inpole.data.preprocessing import make_adni, make_sepsis, make_copd
+from inpole.data.preprocessing import (
+    make_adni,
+    make_ra,
+    make_sepsis,
+    make_copd
+)
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--adni_path', type=str)
+    parser.add_argument('--ra_path', type=str)
     parser.add_argument('--sepsis_path', type=str)
     parser.add_argument('--copd_path', type=str)
-    parser.add_argument('--out_path', type=str)
+    parser.add_argument('--out_path', type=str, default='data')
     args = parser.parse_args()
 
     if args.adni_path:
         make_adni(args.adni_path, args.out_path)
-    
+
+    if args.ra_path:
+        make_ra(args.ra_path, args.out_path)
+
     if args.sepsis_path:
         make_sepsis(args.sepsis_path, args.out_path)
 
