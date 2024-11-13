0d291f512b17083bda877aebe425c5abc9995fcf
antmats <anton.mattsson94@gmail.com>
2024-11-13 10:48:58 +0100

    update slurm script

--- scripts/slurm/perform_model_analysis.sh
+++ scripts/slurm/perform_model_analysis.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-#SBATCH -A NAISS2023-5-397
+#SBATCH -A NAISS2024-5-480
 #SBATCH -p alvis
 #SBATCH -N 1
 #SBATCH --gpus-per-node=A40:1
@@ -23,13 +23,13 @@ if [ "$experiment" == "sepsis" ]; then
     echo "Evaluating sepsis models..."
     apptainer exec --bind $HOME:/mnt --nv $container_path python scripts/perform_model_analysis.py \
         --experiment sepsis \
-        --data_path /mimer/NOBACKUP/groups/inpole/data/sepsis_data_0814.pkl \
+        --data_path /mimer/NOBACKUP/groups/inpole/data/sepsis_data.pkl \
         --out_path /mimer/NOBACKUP/groups/inpole/results
 elif [ "$experiment" == "ra" ]; then
     echo "Evaluating RA models..."
     apptainer exec --bind $HOME:/mnt --nv $container_path python scripts/perform_model_analysis.py \
         --experiment ra \
-        --data_path /mimer/NOBACKUP/groups/inpole/data/ra_cohort_0819.pkl \
+        --data_path /mimer/NOBACKUP/groups/inpole/data/ra_Xgy_incl_cdai.pkl \
         --out_path /mimer/NOBACKUP/groups/inpole/results
 else
     echo "Invalid experiment type. Please choose 'sepsis' or 'ra'."
